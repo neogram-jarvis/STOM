5343d5ad05ca85660cfa5b8ccf682f44f855c7bb
yuseonho <youseonho@naver.com>
2021-10-10 02:41:56 +0900

    업비트 트레이더 주석 왕창 추가

--- coin/trader_upbit.py
+++ coin/trader_upbit.py
@@ -26,7 +26,6 @@ class TraderUpbit:
         self.upbit = None                               # 매도수 주문 및 체결 확인용 객체
         self.buy_uuid = None                            # 매수 주문 저장용 list: [티커명, uuid]
         self.sell_uuid = None                           # 매도 주문 저장용 list: [티커명, uuid]
-        self.websocketQ = None                          # 실시간데이터 수신용 웹소켓큐
 
         self.df_cj = pd.DataFrame(columns=columns_cj)   # 체결목록
         self.df_jg = pd.DataFrame(columns=columns_jg)   # 잔고목록
@@ -47,9 +46,9 @@ class TraderUpbit:
             '실현손익저장': False
         }
         self.dict_time = {
-            '매수체결확인': now(),                          # 1초 마다 매수 체결 확인용
-            '매도체결확인': now(),                          # 1초 마다 매도 체결 확인용
-            '거래정보': now()                              # 잔고목록 및 잔고평가 갱신용
+            '매수체결확인': now(),                        # 1초 마다 매수 체결 확인용
+            '매도체결확인': now(),                        # 1초 마다 매도 체결 확인용
+            '거래정보': now()                            # 잔고목록 및 잔고평가 갱신용
         }
         self.Start()
 
@@ -62,7 +61,8 @@ class TraderUpbit:
     def LoadDatabase(self):
         """
         프로그램 구동 시 당일 체결목록, 당일 거래목록, 잔고목록을 불러온다.
-        체결과 거래목록은 바로 갱신하고 잔고목록은 예수금을 불러온 이후 갱신한다.
+        잔고보유 중 프로그램 구동시 리시버 프로세스가 잔고를 인식할 수 있도록 잔고편입 신호를 보내고
+        디비 체결리스트에 기록된 매수시간을 매수시간 기록용 딕셔너리에 삽입한다.
         """
         con = sqlite3.connect(DB_TRADELIST)
         df = pd.read_sql(f"SELECT * FROM c_chegeollist WHERE 체결시간 LIKE '{self.str_today}%'", con)
@@ -79,21 +79,19 @@ class TraderUpbit:
             self.windowQ.put([ui_num['C거래목록'], self.df_td])
         if len(self.df_jg) > 0:
             for code in self.df_jg.index:
-                self.dict_buyt[code] = now()
+                df = self.df_cj[(self.df_cj['주문구분'] == '매수') & (self.df_cj['종목명'] == code)]
+                self.dict_buyt[code] = strp_time('%Y%m%d%H%M%S%f', df['체결시간'].loc[0])
                 self.creceivQ.put(['잔고편입',  code])
 
         self.windowQ.put([ui_num['C로그텍스트'], '시스템 명령 실행 알림 - 데이터베이스 불러오기 완료'])
 
     def GetKey(self):
         """ 매도수 주문 및 체결확인용 self.upbit 객체 생성 """
-        if DICT_SET['Access_key'] is not None:
-            self.upbit = pyupbit.Upbit(DICT_SET['Access_key'], DICT_SET['Secret_key'])
-            self.windowQ.put([ui_num['C로그텍스트'], '시스템 명령 실행 알림 - 주문 및 체결확인용 업비트 객체 생성 완료'])
-        else:
-            self.windowQ.put([ui_num['C로그텍스트'], '시스템 명령 오류 알림 - 업비트 키값이 설정되지 않았습니다.'])
+        self.upbit = pyupbit.Upbit(DICT_SET['Access_key'], DICT_SET['Secret_key'])
+        self.windowQ.put([ui_num['C로그텍스트'], '시스템 명령 실행 알림 - 주문 및 체결확인용 업비트 객체 생성 완료'])
 
     def GetBalances(self):
-        """ 예수금 조회 및 종목당투자금 계산 """
+        """ 예수금 조회 및 종목당투자금 계산, 계산된 종목당투자금은 전략연산프로세스로 보낸다. """
         if DICT_SET['모의투자2']:
             con = sqlite3.connect(DB_TRADELIST)
             df = pd.read_sql('SELECT * FROM c_tradelist', con)
@@ -124,6 +122,7 @@ class TraderUpbit:
 
     def EventLoop(self):
         while True:
+            """ 주문 및 잔고갱신용 큐를 감시한다. """
             if not self.coinQ.empty():
                 data = self.coinQ.get()
                 if data[0] == '매수':
@@ -131,7 +130,6 @@ class TraderUpbit:
                 elif data[0] == '매도':
                     self.Sell(data[1], data[2], data[3])
                 else:
-                    """ 잔고목록 갱신 및 매도조건 확인 """
                     code, c = data
                     if code in self.df_jg.index:
                         self.UpdateJango(code, c)
@@ -149,7 +147,7 @@ class TraderUpbit:
                 self.UpdateTotaljango()
                 self.dict_time['거래정보'] = timedelta_sec(1)
 
-            """ 0시 초기화 """
+            """ 0시 1분 초기화 """
             if 0 < int(strf_time('%H%M%S')) < 100 and not self.dict_bool['실현손익저장']:
                 self.SaveTotalGetbalDelcjtd()
             time.sleep(0.0001)
@@ -213,6 +211,7 @@ class TraderUpbit:
                 self.cstgQ.put(['매도취소', code])
                 self.windowQ.put([ui_num['C로그텍스트'], f'매매 시스템 오류 알림 - 주문 실패 {code}'])
 
+    """ 리시버가 보내온 현재가와 잔고목록의 현재가가 틀릴 경우만 잔고목록을 갱신하고 매도전략 확인용 데이터를 전략연산 프로세스로 보낸다. """
     def UpdateJango(self, code, c):
         prec = self.df_jg['현재가'][code]
         if prec != c:
@@ -223,6 +222,7 @@ class TraderUpbit:
             self.df_jg.at[code, columns] = c, sp, sg, pg
             self.cstgQ.put([code, sp, oc, c, self.dict_buyt[code]])
 
+    """ 시장가 주문의 체결확인은 리턴값중 체결수량만 확인하여 그 수량이 0을 초과할 경우 매도수를 기록한다. """
     def CheckBuyChegeol(self):
         code = self.buy_uuid[0]
         ret = self.upbit.get_order(self.buy_uuid[1])
@@ -250,11 +250,27 @@ class TraderUpbit:
                 self.UpdateSell(code, cp, cc)
 
     def CheckError(self, ret):
+        """ 주문과 체결확인의 리턴값에 에러가 있을 경우 에러명과 메세지를 로그에 기록한다."""
         if list(ret.keys())[0] == 'error':
             self.windowQ.put([ui_num['C로그텍스트'], f"{ret['error']['name']} : {ret['error']['message']}"])
             return False
         return True
 
+    """
+    매도수 체결완료시 큐로 신호가 인풋되는 과정은 다소 복잡하다.
+    잔고목록의 원장은 트레이더 프로세스가 가지고 있지만,
+    잦은 매수 시그널 발생을 방지하기 위해서 리시버도 잔고목록을 가지고 있어야한다.
+    그래야 리시버가 전략연산 프로세스로 데이터를 보낼때 보유유무 데이터를 보내어
+    전략연산 프로세스가 보유종목에 대해 매수신호 발생을 하지 않도록 한다.
+    그래서 잔고편입 시 트레이더가 리시버로 잔고편입 신호를 보낸다.
+
+    또한 주문은 트레이더가 하지만, 주문목록은 전략연산 프로세스가 가지고 있다.
+    그러므로 트레이더가 체결확인하면 전략연산 프로세스로 매수완료 신호를 보내고
+    그걸 받은 전략연산 프로세스는 주문목록에서 삭제한다.
+
+    체결확인 외 시드부족, 주문실패, 최소주문금액오류, 잔고종목매수신호가 발생하였을 경우도
+    전략연산 프로세스가 가진 주문목록을 삭제하기 위해 매도수 취소 신호를 보낸다. 
+    """
     def UpdateBuy(self, code, cp, cc, cancle=False):
         dt = strf_time('%Y%m%d%H%M%S%f')
         if DICT_SET['모의투자2'] and len(self.df_cj) > 0:
@@ -325,6 +341,7 @@ class TraderUpbit:
         self.teleQ.put(f'매도 알림 - {code} {cp} {cc}')
         self.UpdateTotaltradelist()
 
+    """ 실현손익은 매도체결이 있을 때만, 갱신하여 UI로 보낸다. """
     def UpdateTotaltradelist(self, first=False):
         tsg = self.df_td['매도금액'].sum()
         tbg = self.df_td['매수금액'].sum()
@@ -346,6 +363,7 @@ class TraderUpbit:
         sp = round(sg / bg * 100, 2)
         return pg, sg, sp, bfee, sfee
 
+    """ 1초에 한번 잔고평가를 계산하고 잔고목록 및 잔고평가 데이터를 UI로 보낸다. """
     def UpdateTotaljango(self):
         if len(self.df_jg) > 0:
             tsg = self.df_jg['평가손익'].sum()
@@ -362,7 +380,7 @@ class TraderUpbit:
 
     """
     일별 일현손익 저장, 날짜 변경, 종목당투자금 재계산, 체결목록 및 거래목록 초기화가 진행된다.
-    저장확인용 변수 self.bool_save는 0시 이후 첫번째 매수 주문시 False로 재변경된다.
+    저장확인용 변수 self.bool_save는 0시 1분 이후 첫번째 매수 주문시 False로 재변경된다.
     """
     def SaveTotalGetbalDelcjtd(self):
         df = self.df_tt[['총매수금액', '총매도금액', '총수익금액', '총손실금액', '수익률', '수익금합계']].copy()
