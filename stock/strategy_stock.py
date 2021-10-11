35ca2006ccc72218b2c3f0617f65884aec475fd5
yuseonho <youseonho@naver.com>
2021-10-11 23:52:53 +0900

    큐리스트 길이 오류 수정

--- stock/strategy_stock.py
+++ stock/strategy_stock.py
@@ -66,7 +66,7 @@ class StrategyStock:
             elif type(data) == list:
                 if len(data) == 2:
                     self.UpdateList(data[0], data[1])
-                elif len(data) == 26:
+                elif len(data) == 38:
                     self.BuyStrategy(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8],
                                      data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16],
                                      data[17], data[18], data[19], data[20], data[21], data[22], data[23], data[24],
